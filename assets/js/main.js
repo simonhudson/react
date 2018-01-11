614ee83e59f893f96349444ca4156034fc0acde8
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-01-11 10:34:59 +0000

    Basic component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,19 @@
 'use strict';
 
-var Heading = React.createClass({
-	render: function() {
-		return (
-			<h1>{this.props.text}</h1>
-		);
-	}
-});
+const user = {
+	firstName: 'Joe',
+	lastName: 'Bloggs'
+};
+
+const formatName = (user) => `${user.firstName} ${user.lastName}`;
+
+const Greeting = (
+	<h1>
+		Hello {formatName(user)}
+	</h1>
+);
 
 ReactDOM.render(
-	<Heading text="Hello"/>,
-	document.getElementById('main')
+	Greeting,
+	document.getElementById('greeting')
 );
